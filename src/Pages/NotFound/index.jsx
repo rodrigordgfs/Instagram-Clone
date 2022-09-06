7b772bcb74fe89543b323115a3cb9cad295a280d
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2022-09-06 11:35:57 -0300

    Criada a página not found 404

--- src/Pages/NotFound/index.jsx
+++ src/Pages/NotFound/index.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import Header from "../../Components/Header";
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
-  )
+    <main className="bg-slate-50 min-h-screen">
+      <Header />
+      <div className="flex flex-col items-center justify-center mt-10 px-2">
+        <h1 className="font-semibold text-2xl">
+          Esta página não está disponível.
+        </h1>
+        <p className="text-base mt-3">
+          O link em que você clicou pode não estar funcionando, ou a página pode
+          ter sido removida. <span>Voltar para o Instagram</span>.
+        </p>
+      </div>
+    </main>
+  );
 }
